7dc8889ace5025e48f87f80144ba4029923ff308
Afuwape Babatunde <afutunde@gmail.com>
2025-09-13 15:17:31 +0100

    Add weight conversion functionality
    - Implement conversion methods for kilograms to pounds and vice versa
    - Add proper constructor to set default weight
    - Update package description and name in composer.json

--- src/WeightConversionsClass.php
+++ src/WeightConversionsClass.php
@@ -4,4 +4,20 @@ namespace Afubasic\WeightConversions;
 
 class WeightConversionsClass
 {
+    protected float $kilograms;
+
+    public static function kilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
+
+    public function __construct(float $kilograms)
+    {
+        $this->kilograms = $kilograms;
+    }
+
+    public function toPounds(): float
+    {
+        return $this->kilograms * 2.2046;
+    }
 }
