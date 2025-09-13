033370f117dc9a07024d8aa4daa4d8a42c76deea
AfuBasic <AfuBasic@users.noreply.github.com>
2025-09-13 14:58:43 +0000

    Fix Code Styling

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -2,12 +2,12 @@
 
 use Afubasic\WeightConversions\Weight;
 
-it('can covert kilograms to pounds correctly', function(){
+it('can covert kilograms to pounds correctly', function () {
     $pounds = Weight::kilograms(10)->toPounds();
     expect($pounds)->toEqual(22.046);
 });
 
-it('can convert to stones correctly', function(){
+it('can convert to stones correctly', function () {
     $stones = Weight::kilograms(100)->toStones();
     expect($stones)->toEqual(15.747);
 });
